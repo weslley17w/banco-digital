42effd1a754fc70bce129c81b3c03331ec311241
Weslley Barros <weslley17w@gmail.com>
2022-02-26 12:56:13 -0300

    Add methods sacar, depositar, transferir

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -3,4 +3,10 @@ public class ContaCorrente {
     public int agencia;
     public int numero;
     public double saldo;
+
+    public void sacar(){}
+
+    public void depositar(){}
+
+    public void transferir(){}
 }
--- src/ContaPopanca.java
+++ src/ContaPopanca.java
@@ -4,4 +4,10 @@ public class ContaPopanca {
     public int numero;
     public double saldo;
 
+    public void sacar(){}
+
+    public void depositar(){}
+
+    public void transferir(){}
+
 }

3cf3c6d9c14bccd6b538494b90ed52d362f6eddd
Weslley Barros <weslley17w@gmail.com>
2022-02-26 13:11:18 -0300

    Add get and set

--- src/Banco.java
+++ src/Banco.java
@@ -2,5 +2,11 @@ public class Banco {
 
     private String nome;
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/ContaPopanca.java
+++ src/ContaPopanca.java
@@ -9,4 +9,16 @@ public class ContaPopanca {
     public void depositar(){}
 
     public void transferir(){}
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
